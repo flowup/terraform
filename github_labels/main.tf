b85a1cefc70701774c7b3f12084aed9d6fea1d3e
Peter Malina <peter.malina@flowup.cz>
2018-09-21 13:43:03 +0200

    docs(github_labels): added docs

--- github_labels/main.tf
+++ github_labels/main.tf
@@ -1,13 +1,53 @@
+# provider "github" {
+#   token        = ""
+#   organization = ""
+# }
+#
 # module "service_account" {
 #   source = "git::git@github.com:flowup/terraform.git//github_labels?ref=master"
-#   name   = "${var.name}-gke"
+#   repo   = "${var.repo}"
 #
-#   feat = [
-#     "landing"
+#   general = {
+#     "bug" = "d73a4a"
+#     "regression" = "c11325"
+#     "feature" = "6cc138"
+#     "enhancement" = "b0ea8c"
+#   }
+#   layers = {
+#     "front-end" = "1d76db"
+#     "back-end" = "f97625"
+#     "infrastructure" = "FFD700"
+#   }
+#   feats = [
+#     "landing",
+#     "onboarding",
+#     "dashboard",
 #   ]
-#
-#   util = [
-#     "responsivity"
+#   utils = [
+#     "documentation",
+#     "responsivity",
+#     "tests",
+#   ]
+#   estimates = [
+#     "1",
+#     "2",
+#     "3",
+#     "5",
+#     "8",
+#     "13",
+#     "21",
+#     "40",
+#   ]
+#   frequencies = [
+#     "low",
+#     "medium",
+#     "high",
+#   ]
+#   severities = [
+#     "trivial",
+#     "minor",
+#     "major",
+#     "critical",
 #   ]
 # }
 
